d6cb35c88e277572beda357b0b44c30f7ea617de
Kaue Pereira <kauefontes@outlook.com>
2022-11-01 20:29:25 -0400

    feat: added home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from 'native-base'
+
+export function Home(){
+  return(
+    <Box flex={1} alignItems='center' justifyContent='center'>
+      Hello Beer!
+    </Box>
+  )
+}
